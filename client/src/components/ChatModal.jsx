f54523b09541b494cde8e0ab7b7dd2e44fe77ce3
ronak-pal1 <ronakpaul882@gmail.com>
2025-01-04 18:25:23 +0530

    endpoint fixed

--- client/src/components/ChatModal.jsx
+++ client/src/components/ChatModal.jsx
@@ -14,7 +14,7 @@ const ChatModal = ({ isHidden, setIsHidden }) => {
   const fetchAIResponse = async (prompt) => {
     setIsThinking(true);
     try {
-      const response = await fetch("http://localhost:8000/chat", {
+      const response = await fetch("https://postpulse.ronakpaul.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
